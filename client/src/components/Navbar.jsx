21d458efc7fb26051cd54d14935547459eced50e
shiva8081 <yadavshiva8755@gmail.com>
2024-07-06 21:37:07 +0530

    login signup done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = ({AuthUser}) => {
   return (
     
     <div className='bg-slate-500 text-white flex h-[50px] justify-between items-center w-full '>
-      <span className='ml-5 '><Link to="/">lame app</Link></span>
+      <span className='ml-5 '><Link to="/">Lame app</Link></span>
       {
         AuthUser
         ?
         ( <ul className="list flex space-x-5 mr-5">
-          <li className=""><img className='w-[32px] h-[32px] rounded-full' src={AuthUser.photos[0].value} alt="hello" /></li>
-          <li className="">{AuthUser.displayName}</li>
+          <li className=""><img className='w-[32px] h-[32px] rounded-full' src="https://avatar.iran.liara.run/public" alt="hello" /></li>
+          <li className="">shiva</li>
           <li className="cursor-pointer "onClick={logout} >logout</li>
         </ul>)
         :
